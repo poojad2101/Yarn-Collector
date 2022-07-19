6fff8df3573cbfcf53a96177e6a6f5965c22f224
poojad2101 <davepooja2101@gmail.com>
2022-07-20 02:07:33 +0530

    added superuser

--- yarncollector/main_app/models.py
+++ yarncollector/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Yarn(models.Model):
+    color = models.CharField(max_length=100)
+    weight = models.CharField(max_length=100)
+    brand = models.TextField(max_length=250)
+    
--- yarncollector/main_app/views.py
+++ yarncollector/main_app/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 
 # Add the yarn class & list and view function below the imports
-class Yarn:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, color, weight, brand, ):
-    self.color = color
-    self.weight = weight
-    self.brand = brand
+# class Yarn:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, color, weight, brand):
+#     self.color = color
+#     self.weight = weight
+#     self.brand = brand
 
 
-yarns = [
-  Yarn('White', 'worsted weight', 'Lion brand'),
-  Yarn('Red', 'bulky', 'bernat'),
-  Yarn('Varigated', 'fingering weight', 'Caron')
-]
+# yarns = [
+#   Yarn('White', 'worsted weight', 'Lion brand'),
+#   Yarn('Red', 'bulky', 'bernat'),
+#   Yarn('Varigated', 'fingering weight', 'Caron')
+# ]
 
 from django.http import HttpResponse
+from .models import Yarn
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Define the home view
 def home(request):
@@ -25,4 +28,5 @@ def about(request):
 
   # Add new view
 def yarns_index(request):
+  yarns = Yarn.objects.all()
   return render(request, 'yarns/index.html', { 'yarns': yarns })
